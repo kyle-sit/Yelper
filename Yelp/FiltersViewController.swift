879bbe3c8f8e848d62fd3f914af7a99e01d2b943
Kyle Sit <ktsit@ucsd.edu>
2017-07-25 14:55:39 -0700

    filters view controller changed format

--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FiltersViewController: UIViewController {
 
     //outlets
-    @IBOutlet weak var filtersTableView1: UITableView!
-    @IBOutlet weak var filtersTableView2: UITableView!
+    @IBOutlet weak var orderDeliverySwitch: UISwitch!
+    @IBOutlet weak var orderPickupSwitch: UISwitch!
+    @IBOutlet weak var makeReservationSwitch: UISwitch!
+    @IBOutlet weak var hotAndNewSwitch: UISwitch!
+    @IBOutlet weak var openNowSwitch: UISwitch!
+    @IBOutlet weak var offeringDealsSwitch: UISwitch!
     
     //category variables for searching
     var orderDelivery = false
@@ -29,17 +33,19 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
     //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //TableViews setup
-        filtersTableView1.delegate = self
-        filtersTableView1.dataSource = self
-        filtersTableView2.delegate = self
-        filtersTableView2.dataSource = self
         
         //UI specifications for navigation bar red
         if let navigationBar = navigationController?.navigationBar {
             navigationBar.barTintColor = UIColor(red: 0.9176, green: 0, blue: 0, alpha: 1.0)
         }
+        
+        //setting all switches off
+        orderDeliverySwitch.isOn = false
+        orderPickupSwitch.isOn = false
+        makeReservationSwitch.isOn = false
+        hotAndNewSwitch.isOn = false
+        openNowSwitch.isOn = false
+        offeringDealsSwitch.isOn = false
         
         //setting all search vars to false
         orderDelivery = false
@@ -48,109 +54,6 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
         offeringDeal = false
         hotAndNew = false
         openNow = false
-    }
-
-    
-    //function to determine rows count
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-
-
-    //function to populate cells
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        var cell : UITableViewCell?
-        
-        //First Filters TableView
-        if tableView == self.filtersTableView1{
-            let cell1 = filtersTableView1.dequeueReusableCell(withIdentifier: "FilterCell1", for: indexPath) as! FilterCell
-            
-            //turn switch off
-            cell1.switch1.isOn = false
-           
-            //individually name each option
-            if indexPath.row == 1 {
-                cell1.label1.text = "Order Delivery"
-            }
-            else if indexPath.row == 2 {
-                cell1.label1.text = "Order Pickup"
-            }
-            else {
-                cell1.label1.text = "Make a Reservation"
-            }
-            cell = cell1
-        }
-        
-        //Second Filters TableView
-        if tableView == self.filtersTableView2 {
-            let cell2 = filtersTableView2.dequeueReusableCell(withIdentifier: "FilterCell2", for: indexPath) as! FilterCell
-            
-            //turn switch off
-            cell2.switch2.isOn = false
-            
-            //individually name each option
-            if indexPath.row == 1 {
-                cell2.label2.text = "Offering a Deal"
-            }
-            else if indexPath.row == 2 {
-                cell2.label2.text = "Hot and New"
-            }
-            else {
-                cell2.label2.text = "Open Now"
-            }
-            cell = cell2
-        }
-        
-        return cell!
-    }
-    
-    
-    //turn on options for first 3 switches
-    @IBAction func OnSwitch1(_ sender: Any) {
-        let cell1 = filtersTableView1.dequeueReusableCell(withIdentifier: "FilterCell1") as! FilterCell
-        
-        //individually name each option
-        if cell1.label1.text == "Order Delivery" {
-            if cell1.switch1.isOn == true {
-                self.orderDelivery = true
-                print(orderDelivery)
-            }
-        }
-        else if cell1.label1.text == "Order Pickup" {
-            if cell1.switch1.isOn == true {
-                self.orderPickup = true
-                print(orderPickup)
-            }
-        }
-        else if cell1.label1.text == "Make a Reservation" {
-            if cell1.switch1.isOn == true {
-                self.makeReservation = true
-            }
-        }
-    }
-    
-    
-    //turn on options for second 3 swtiches
-    @IBAction func OnSwitch2(_ sender: Any) {
-        let cell2 = filtersTableView2.dequeueReusableCell(withIdentifier: "FilterCell2") as! FilterCell
-        
-        //individually name each option
-        if cell2.label2.text == "Offering a Deal" {
-            if cell2.switch2.isOn == true {
-                self.offeringDeal = true
-            }
-        }
-        else if cell2.label2.text == "Hot and New" {
-            if cell2.switch2.isOn == true {
-                self.hotAndNew = true
-            }
-        }
-        else if cell2.label2.text == "Open Now" {
-            if cell2.switch2.isOn == true {
-                self.openNow = true
-            }
-        }
     }
     
     
@@ -174,6 +77,54 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
         print(orderDelivery)
         print(orderPickup)
         //self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    //onSwitch for delivery
+    @IBAction func OnSwitchOrDel(_ sender: Any) {
+        if orderDeliverySwitch.isOn == true {
+            orderDelivery = true
+        }
+    }
+    
+    
+    //onSwitch for pickup
+    @IBAction func OnSwitchOrPic(_ sender: Any) {
+        if orderPickupSwitch.isOn == true {
+            orderPickup = true
+        }
+    }
+    
+    
+    //onSwitch for reservation
+    @IBAction func OnSwitchMkRes(_ sender: Any) {
+        if makeReservationSwitch.isOn == true {
+            makeReservation = true
+        }
+    }
+    
+    
+    //onSwitch for deals
+    @IBAction func OnSwitchOfDeal(_ sender: Any) {
+        if offeringDealsSwitch.isOn == true {
+            offeringDeal = true
+        }
+    }
+    
+    
+    //onSwitch for hot and new
+    @IBAction func OnSwitchHAN(_ sender: Any) {
+        if hotAndNewSwitch.isOn == true {
+            hotAndNew = true
+        }
+    }
+    
+    
+    //onSwitch for open now
+    @IBAction func OnSwitchON(_ sender: Any) {
+        if openNowSwitch.isOn == true {
+            openNow = true
+        }
     }
     
     
